e2ecbb27a749829946637373d6a74775f20cb301
RyanRigoto <164576343+RyanRIGOTO@users.noreply.github.com>
2026-04-11 01:08:34 +0000

    Criando Migrations

---
+++ maridaodealuguel/app/Http/Controllers/RecebimentoController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\recebimento;
+use Illuminate\Http\Request;
+
+class RecebimentoController
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(recebimento $recebimento)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(recebimento $recebimento)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, recebimento $recebimento)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(recebimento $recebimento)
+    {
+        //
+    }
+}
